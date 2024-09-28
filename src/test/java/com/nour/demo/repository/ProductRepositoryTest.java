16427d857fb66a0c002e578e6a82f629549f63a6
nour <174273417+NourDoesCode@users.noreply.github.com>
2024-09-28 20:23:22 +0100

    add saveAllMethod test method

--- src/test/java/com/nour/demo/repository/ProductRepositoryTest.java
+++ src/test/java/com/nour/demo/repository/ProductRepositoryTest.java
@@ -1,18 +1,20 @@
 package com.nour.demo.repository;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.nour.demo.model.Product;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-// @Transactional(propagation = Propagation.NOT_SUPPORTED) // Prevents rollback
-// after test
+
 public class ProductRepositoryTest {
 
     @Autowired
@@ -54,5 +56,29 @@ public class ProductRepositoryTest {
         Long id = 3L;
         Product product = productRepository.findById(id).get();
 
+    }
+
+    @Transactional(propagation = Propagation.NOT_SUPPORTED) // Prevents rollback after test
+    @Test
+    void saveAllMethod() {
+        // create product1
+        Product product = new Product();
+        product.setName("Product 2");
+        product.setDescription("description of Product 1");
+        product.setStock("zee44");
+        product.setPrice(BigDecimal.valueOf(100));
+        product.setImageUrl("product2.png");
+        product.setActive(false);
+
+        // create product2
+        Product product3 = new Product();
+        product3.setName("Product3");
+        product3.setDescription("description of Product3 ");
+        product3.setStock("f3ef44");
+        product3.setPrice(BigDecimal.valueOf(780));
+        product3.setImageUrl("product3.png");
+        product3.setActive(true);
+
+        productRepository.saveAll(List.of(product, product3));
     }
 }
